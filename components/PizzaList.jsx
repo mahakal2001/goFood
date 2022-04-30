a6efc596de8c8d4d05a76e59c764fa3f5b08e872
mahakal2001 <bhaktabhim2001@gmail.com>
2022-04-30 11:52:42 +0530

    make it better

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,11 +4,9 @@ import PizzaCard from "./PizzaCard";
 const PizzaList = ({ pizzaList }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <h1 className={styles.title}>THE BEST PIZZA IN KOLKATA</h1>
       <p className={styles.desc}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
-        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit.
+      If you love pampering your taste buds with the best pizza flavors in town, you’ll fall in love with the exciting range of veg Domino’s pizzas. From freshly sourced veggies to 100% authentic mozzarella cheese- our veg pizzas have everything to satisfy your pizza cravings. 
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
