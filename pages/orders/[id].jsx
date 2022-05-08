aed45331cd402837bea8d38842bda797e2cbcbcf
mahakal2001 <bhaktabhim2001@gmail.com>
2022-05-08 19:27:10 +0530

    MongoDb error handeling

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -114,7 +114,7 @@ const Order = ({ order }) => {
 };
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+  const res = await axios.get(`${process.env.API_URL}/api/orders/${params.id}`);
   return {
     props: { order: res.data },
   };
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -97,7 +97,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `${process.env.API_URL}/api/products/${params.id}`
   );
   return {
     props: {
